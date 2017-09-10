460ce81913cde0e7bf38cf7c3278038d69498555
Ryan Bie <ryanbie@Ryans-MacBook-Air.local>
2017-09-09 21:24:38 -0400

    Added Camera Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    // target is initialized to the GameObject Player
+    public GameObject target;
+
+    // Amount that we want the camera to follow ahead of the Player
+    public float followahead;
+
+    private Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Object is attached to Main Camera -> Player
+        targetPosition = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+        // x value: Will move the camera to follow the Player on the x-axis
+        // y value: Will keep the camera at the same height by taking the MainCamera's current y-position 
+        // z value: Will take the MainCamera's current z-position
+
+        if (target.transform.localScale.x > 0f) // Player is facing right
+        {
+            else if (target.transform.localScale.x < 0f) // Player is facing left 
+        }
+	}
+}

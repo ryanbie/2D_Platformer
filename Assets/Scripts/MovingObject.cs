e7075751cd7e95f361b2acd20bb19fc14054a7c6
Ryan Bie <ryanbie@Ryans-MacBook-Air.local>
2017-09-19 23:11:19 -0400

    Created and Added a Moving Platform GameObject(and its children) and created and added a MovingObject script

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour {
+
+    public GameObject objectToMove;
+
+    public Transform startPoint;
+    public Transform endPoint;
+
+    public float moveSpeed; // How fast the platform will move
+
+    private Vector3 currentTarget; // Where the platform should end/meet (Target) 
+
+	// Use this for initialization
+	void Start () {
+        currentTarget = endPoint.position; // The Target (for the platform) will be the endPoint's position
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        objectToMove.transform.position = Vector3.MoveTowards(objectToMove.transform.position, currentTarget, moveSpeed * Time.deltaTime); // The platform should move from its current position to the currentTarget position at a certain movement speed and FPS. 
+
+        /* If the platform's position is equal to the endPoint's position, 
+         * the currentTarget will shift back to the starting position. 
+         * Thus, the platform should end/meet at the startPoint position.
+         */ 
+       
+        if(objectToMove.transform.position == endPoint.position)
+        {
+            currentTarget = startPoint.position; 
+        }
+
+	   /* However, if the platform's position is equal to the startPoint's position, 
+		* the currentTarget will shift back to the ending position. 
+		* Thus, the platform should end/meet at the endPoint position.
+		*/
+
+		if(objectToMove.transform.position == startPoint.position)
+        {
+            currentTarget = endPoint.position;
+        }
+    }
+}
